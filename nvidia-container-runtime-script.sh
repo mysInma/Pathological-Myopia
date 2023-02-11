19a3e1b9681a415e955192437a0aff4a00c4a5ec
marnugue <marcostorterra@gmail.com>
2023-02-11 15:47:23 +0100

    dockerized project

---
+++ nvidia-container-runtime-script.sh
@@ -0,0 +1,10 @@
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
+  sudo apt-key add -
+distribution=ubuntu20.04
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+sudo apt-get update
+apt-get install nvidia-container-runtime
+apt -y install nvidia-container-toolkit 
+nvidia-ctk runtime configure --runtime=docker
+systemctl restart docker 
