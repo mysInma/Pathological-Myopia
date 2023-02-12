67387c6139bf8f874ceb5d17a154ae96a4a10441
marnugue <marcostorterra@gmail.com>
2023-02-12 19:29:35 +0100

    dataloader read images

--- notebooks/utils/createCSV.py
+++ notebooks/utils/createCSV.py
@@ -6,6 +6,13 @@ def createCSV(dir_path,outputPath):
     df = pd.DataFrame(columns=["index","imgPath","label"])
     for index, img_path in enumerate(glob(os.path.join(dir_path,"*.jpg"))):
         file_name = os.path.basename(img_path) #para extraer el nombre de archivo de img_path.
-        label = file_name.split("_")[0][0]
+        label = encodingLabel(file_name.split("_")[0][0])
         df.loc[index] = [index,img_path,label]
     df.to_csv(outputPath,index=False)
+def encodingLabel(label):
+    if "H" in label:
+        return 0
+    elif "N" in label:
+        return 1
+    else:
+        return 2
