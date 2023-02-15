2fc18ec14ec8e2f6e49aba799839696b6d500385
mysInma <inmamayo00@gmail.com>
2023-02-15 22:31:17 +0100

    updates transformations

--- notebooks/utils/transformations.py
+++ notebooks/utils/transformations.py
@@ -2,19 +2,16 @@ import torch
 import torchvision.transforms as transforms
 from torchvision.transforms import Compose
 
-class CustomTransformations(Compose):
+class CustomTransformations(self):
     def __init__(self, mean, std):
         self.mean = mean
         self.std = std
         self.transformations = transforms.Compose([
-            transforms.Resize((224,224)),
-            transforms.RandomCrop((224,224)),
-            transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
-            transforms.RandomChoice([transforms.RandomHorizontalFlip(),transforms.RandomVerticalFlip()]),
-            transforms.RandomRotation(degrees=30),
-            transforms.RandomGrayscale(p=0.2),
-            transforms.Normalize(self.mean, self.std)
+             transforms.RandomHorizontalFlip(),
+            transforms.RandomAffine(degrees=0, translate=(0.2, 0.2), scale=(0.8, 1.2)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std)
         ])
         
-    def __call__(self, tensor):
-        return self.transformations(tensor)
+    def __call__(self, img):
+        return self.transform(img)
