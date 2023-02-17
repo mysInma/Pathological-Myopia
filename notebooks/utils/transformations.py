ac5b04cdf338b657055ba4e9e13f3741fb20ce5f
marnugue <marcostorterra@gmail.com>
2023-02-17 21:46:56 +0100

    transformation Customized

--- notebooks/utils/transformations.py
+++ notebooks/utils/transformations.py
@@ -2,16 +2,22 @@ import torch
 import torchvision.transforms as transforms
 from torchvision.transforms import Compose
 
-class CustomTransformations(self):
-    def __init__(self, mean, std):
-        self.mean = mean
-        self.std = std
-        self.transformations = transforms.Compose([
-             transforms.RandomHorizontalFlip(),
-            transforms.RandomAffine(degrees=0, translate=(0.2, 0.2), scale=(0.8, 1.2)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean, std)
-        ])
+class CustomTransformations():
+    def __init__(self, size):
+        self.size = size
         
     def __call__(self, img):
-        return self.transform(img)
+        orSize = (img.shape[1],img.shape[2])
+        return transforms.Compose([
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            transforms.RandomChoice([
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomVerticalFlip(),
+                transforms.RandomResizedCrop(size=orSize,scale=(0.4,0.9)),
+                transforms.Compose([
+                transforms.RandomResizedCrop(size=orSize[0]//1.2,scale=(0.4,0.9)),
+                transforms.Pad(int(self.size-(self.size//1.2)),fill=255)
+                ])
+            ]),
+            transforms.Resize((self.size,self.size))
+        ])(img)
