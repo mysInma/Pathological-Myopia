c28cd24fefc1e1c887591276c6d57158f7cf8dfd
marnugue <marcostorterra@gmail.com>
2023-02-17 21:46:56 +0100

    transformation customized sq develop

--- notebooks/utils/transformations.py
+++ notebooks/utils/transformations.py
@@ -2,20 +2,22 @@ import torch
 import torchvision.transforms as transforms
 from torchvision.transforms import Compose
 
-class CustomTransformations(Compose):
-    def __init__(self, mean, std):
-        self.mean = mean
-        self.std = std
-        self.transformations = transforms.Compose([
-            transforms.Resize((224,224)),
-            transforms.RandomCrop((224,224)),
-            transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
-            transforms.RandomChoice([transforms.RandomHorizontalFlip(),transforms.RandomVerticalFlip()]),
-            transforms.RandomRotation(degrees=30),
-            transforms.RandomGrayscale(p=0.2),
-            transforms.ToTensor(),
-            transforms.Normalize(self.mean, self.std)
-        ])
+class CustomTransformations():
+    def __init__(self, size):
+        self.size = size
         
-    def __call__(self, tensor):
-        return self.transformations(tensor)
+    def __call__(self, img):
+        orSize = (img.shape[1],img.shape[2])
+        return transforms.Compose([
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            transforms.RandomChoice([
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomVerticalFlip(),
+                transforms.RandomResizedCrop(size=orSize,scale=(0.4,0.9)),
+                transforms.Compose([
+                transforms.RandomResizedCrop(size=orSize[0]//1.2,scale=(0.4,0.9)),
+                transforms.Pad(int(self.size-(self.size//1.2)),fill=255)
+                ])
+            ]),
+            transforms.Resize((self.size,self.size))
+        ])(img)
