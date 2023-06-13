b1e3c4b82a3b0c4c31200b612cade7425c509927
mysInma <inmamayo00@gmail.com>
2023-06-13 18:32:49 +0200

    changes in metrics

--- notebooks/models/efficient_resunet.py
+++ notebooks/models/efficient_resunet.py
@@ -2,7 +2,7 @@ import pytorch_lightning as pl
 from torch import Tensor
 from torch import optim, nn
 from torch.nn import functional as F
-from torchmetrics import Accuracy   
+from torchmetrics import Accuracy
 from torch.nn import Conv2d, MaxPool2d, Upsample, ReLU, BatchNorm2d, Sequential
 from torchmetrics.classification import BinaryRecall, BinaryAUROC
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
@@ -18,12 +18,12 @@ class ConvBlock(nn.Module):
     self.c1 = Conv2d(input_channel,mid_channels,kernel_size=3,padding=1)
     self.b1 = BatchNorm2d(mid_channels)
     self.r1 = ReLU()
-  
+
   def forward(self,x):
         x = self.c1(x)
         x = self.b1(x)
         return self.r1(x)
-    
+
 class Encoder_residual_block(nn.Module):
     def __init__(self, input_channels, mid_channels):
       super().__init__()
@@ -32,7 +32,7 @@ class Encoder_residual_block(nn.Module):
       self.c2 = Conv2d(mid_channels,mid_channels,kernel_size=3,padding=1)
       self.r1 = ReLU()
       self.r2 = ReLU()
-    
+
     def forward(self,x0):
       x = self.c1(x0)
       x = self.r1(x)
@@ -51,19 +51,19 @@ class Intermediate(nn.Module):
     self.conv_block_4 = ConvBlock(self.img_size*2,self.img_size*2)
     self.conv_block_5 = ConvBlock(self.img_size*2,self.img_size)
     self.conv_block_6 = ConvBlock(self.img_size,self.img_size)
-    
+
     self.encoder_residual_block = Encoder_residual_block(self.img_size,self.img_size)
     self.decoder_residual_block_1 = Decoder_residual_block(self.img_size*2,self.img_size)
     self.decoder_residual_block_2 = Decoder_residual_block(self.img_size,self.img_size//2)
-    
+
     self.max_pool = MaxPool2d(2)
-    
-  
+
+
   def forward(self,x):
-    
+
         # x = self.convBlock(self.img_size//2,self.img_size,features_encoder[-1][1])
         # x = self.convBlock(self.img_size,self.img_size,x)
-        
+
         x = self.conv_block_1(x)
         x = self.conv_block_2(x)
 
@@ -74,20 +74,20 @@ class Intermediate(nn.Module):
 
         # x1 = self.convBlock(self.img_size,self.img_size*2,x1)
         # x1 = self.convBlock(self.img_size*2,self.img_size*2,x1)
-        
+
         x1 = self.conv_block_3(x1)
         x1 = self.conv_block_4(x1)
-        
+
 
         # x1 = self.decoder_residualBlock(self.img_size*2,self.img_size,x1)
         x1 = self.decoder_residual_block_1(x1)
-        
+
         x = torch.cat([x,x1],dim=1)
         del x1
 
         # x = self.convBlock(self.img_size*2,self.img_size,x)
         # x = self.convBlock(self.img_size,self.img_size,x)
-        
+
         x = self.conv_block_5(x)
         x = self.conv_block_6(x)
 
@@ -105,7 +105,7 @@ class Decoder_residual_block(nn.Module):
       self.c3 = Conv2d(mid_channels,mid_channels,kernel_size=3,padding=1)
       self.skip = Conv2d(input_channels,mid_channels,kernel_size=1,padding=0)
       self.upsample = Upsample(scale_factor=2,mode="bilinear",align_corners=True)
-    
+
   def forward(self,x0):
       x = self.c1(x0)
       x = self.r1(x)
@@ -120,93 +120,93 @@ class Decoder_residual_block(nn.Module):
 class EfficientResUNET(nn.Module):
     def __init__(self):
         super().__init__()
-        
-        
+
+
         # Encoder
-        
+
         # lv1
         self.e1 = ConvBlock(3,64)
         self.e11 = ConvBlock(64,64)
         self.e1_res_block = Encoder_residual_block(64,64)
         self.e1_down = MaxPool2d(2)
-        
+
         #lv2
         self.e2 = ConvBlock(64,128)
         self.e22 = ConvBlock(128,128)
         self.e2_res_block = Encoder_residual_block(128,128)
         self.e2_down = MaxPool2d(2)
-        
+
         #lv3
         self.e3 = ConvBlock(128,256)
         self.e33 = ConvBlock(256,256)
         self.e3_res_block = Encoder_residual_block(256,256)
         self.e3_down = MaxPool2d(2)
-        
+
         self.intermediate = Intermediate(512)
-        
+
         # Decoder
-        
+
         #lv3
         self.d3 = ConvBlock(512,256)
         self.d33 = ConvBlock(256,256)
         self.d3_res_block = Decoder_residual_block(256,128)
-        
+
         #lv2
         self.d2 = ConvBlock(256,128)
         self.d22 = ConvBlock(128,128)
         self.d2_res_block = Decoder_residual_block(128,64)
-        
+
         #lv1
         self.d1 = ConvBlock(128,64)
         self.d11 = ConvBlock(64,1)
-        
-    
+
+
     def forward(self,x):
-        
+
         #Encoder
-        
+
         #lv1
         x = self.e1(x)
         x = self.e11(x)
         x_f1 = self.e1_res_block(x)
         x = self.e1_down(x_f1)
-        
+
         #lv2
         x = self.e2(x)
         x = self.e22(x)
         x_f2 = self.e2_res_block(x)
         x = self.e2_down(x_f2)
-        
+
         #lv3
         x = self.e3(x)
         x = self.e33(x)
         x_f3 = self.e3_res_block(x)
         x = self.e3_down(x_f3)
-        
+
         # Intermediate
         x = self.intermediate(x)
-        
+
         # Decoder
-        
+
         # lv3
         x = torch.cat([x,x_f3],dim=1)
         x = self.d3(x)
         x = self.d33(x)
         x = self.d3_res_block(x)
-        
+
         # lv2
         x = torch.cat([x,x_f2],dim=1)
         x = self.d2(x)
         x = self.d22(x)
         x = self.d2_res_block(x)
-        
+
         # lv1
         x = torch.cat([x,x_f1],dim=1)
         x = self.d1(x)
         x = self.d11(x)
-        
+
         del x_f1,x_f2,x_f3
-        
+
         return x
 
 # if __name__ == '__main__':
@@ -218,103 +218,125 @@ class EfficientResUNET(nn.Module):
 
 class SegmentationModel(pl.LightningModule):
     def __init__(self, config):
-        super().__init__()
+        super().__init__() 
         self.save_hyperparameters(config)
         self.model = EfficientResUNET()
         # self.model.encoder.features = self.model.encoder.features.to(self.device)
         self.accuracy = Accuracy(task="binary")
         self.auc =  BinaryAUROC()
         self.criterion = nn.BCEWithLogitsLoss()
-       # self.recall = BinaryRecall(average="macro") 
-       
-        self.train_acc_list = []
-        self.train_auroc_list = []
+       # self.recall = BinaryRecall(average="macro")
+
+        self.loss_train_tensor = torch.Tensor() # Tensor para almacenar los valores de los steps
+        self.accuracy_train_tensor = torch.Tensor()  # Tensor para almacenar los valores de accuracy
+        self.auroc_train_tensor = torch.Tensor()  # Tensor para almacenar los valores de auroc
+
+        self.loss_train_list = []  # Lista para almacenar los valores de loss
+        self.accuracy_train_list = []  # Lista para almacenar los valores de accuracy
+        self.auroc_train_list = []  # Lista para almacenar los valores de auroc
         
-        self.avg_train_acc_list = []
-        self.avg_train_auroc_list = []
         
+        self.loss_val_tensor = torch.Tensor()
+        self.accuracy_val_tensor = torch.Tensor()
+        self.auroc_val_tensor = torch.Tensor()
         
-        self.val_acc_list = []
-        self.val_auroc_list = []
-          
-        self.avg_val_acc_list = []
-        self.avg_val_auroc_list = []
-          
+        self.loss_val_list = []
+        self.accuracy_val_list = []
+        self.auroc_val_list = []
+        
+
+
     def forward(self, x):
         return self.model(x)
-    
+
+
     def training_step(self, batch, batch_idx):
-    
       x, y = batch
-      # x = x.to(self.device)
-      # y = y.to(self.device)
       logits = self(x)
       yhat = torch.sigmoid(logits)
       loss = F.binary_cross_entropy(yhat, y)
       loss += self.dice_coeff(yhat.squeeze(1),y.squeeze(1))
-      
+
+      accuracy = self.accuracy(yhat, y)
+      auroc = self.auc(yhat, y)
+
       self.log("train_loss_step", loss,prog_bar=True,on_epoch=True,on_step=False)
       # self.log("train_acc_step", self.accuracy(yhat, y),prog_bar=True,on_epoch=True,on_step=False)
       # self.log("train_auroc_step", self.auc(yhat, y),prog_bar=True,on_epoch=True,on_step=False)
       #self.log("train_recall_step",self.recall(torch.round(yhat* torch.pow(10, torch.tensor(2))) / torch.pow(10, torch.tensor(2)),y),on_epoch=True,on_step=False)
       # self.log("train_recall_step",self.recall(torch.argmax(yhat,dim=1),y),on_epoch=True,on_step=False)
-      
 
-      self.train_acc_list.append(self.accuracy(yhat, y).item())
-      self.train_auroc_list.append(self.auc(yhat, y).item())
-      
+      self.loss_train_tensor = torch.cat([self.loss_train_tensor, loss])
+      self.accuracy_train_tensor = torch.cat([self.accuracy_train_tensor, accuracy])
+      self.auroc_train_tensor = torch.cat([self.auroc_train_tensor, auroc])
+
       return loss
-    
-    def training_epoch_end(self, training_step_outputs):
-      
-      # Calcula la media de los tensores
-      avg_acc = torch.tensor(self.train_acc_list).mean()
-      avg_auc = torch.tensor(self.train_auroc_list).mean()
 
-      self.train_acc_list.clear()
-      self.train_auroc_list.clear()
-      
-      self.avg_train_acc_list.append(avg_acc.item())
-      self.avg_train_auroc_list.append(avg_auc.item())
-    
-    
+
+    def training_epoch_end(self, training_step_outputs): 
+
+      # Calcula la media de los tensores
+      avg_loss = self.loss_train_tensor.mean().item()
+      avg_accuracy = self.accuracy_train_tensor.mean().item()
+      avg_auroc = self.auroc_train_tensor.mean().item()
+
+      #Opción para llevarmelos a la cpu
+      # avg_accuracy = self.accuracy_train_tensor.cpu().mean().item()
+      # avg_auroc = self.auroc_train_tensor.cpu().mean().item()
+
+      self.loss_train_tensor = []
+      self.accuracy_train_tensor = []
+      self.auroc_train_tensor = []
+
+      self.loss_train_list.append(avg_loss)  # Agregar el valor de avg_loss a la lista loss_train_list
+      self.accuracy_train_list.append(avg_accuracy)  # Agregar el valor de avg_accuracy a la lista accuracy_train_list
+      self.auroc_train_list.append(avg_auroc)  # Agregar el valor de avg_auroc a la lista auroc_train_list
+
+
       #self.log("step",self.current_epoch)
-        
-        
+
+
     def validation_step(self, batch, batch_idx):
-      
       x,y = batch
       logits = self(x)
       yhat = torch.sigmoid(logits)
       loss = F.binary_cross_entropy(yhat, y)
       loss += self.dice_coeff(yhat.squeeze(1),y.squeeze(1))
-        
+
       self.log("train_val_loss", loss,prog_bar=True)
       # self.log("train_val_acc", self.accuracy(logits, y),prog_bar=True)
       # self.log("train_val_auroc", self.auc(yhat, y),prog_bar=True)
       #self.log("train_val_recall",self.recall(torch.round(yhat* torch.pow(10, torch.tensor(2))) / torch.pow(10, torch.tensor(2)),y))
-        
-      self.val_acc_list.append(self.accuracy(logits, y).item())
-      self.val_auroc_list.append(self.auc(yhat, y).item())
 
+      accuracy_val = self.accuracy(logits, y)
+      auroc_val = self.auc(yhat, y)
+      
+      self.loss_val_tensor = torch.cat([self.loss_val_tensor, loss])
+      self.accuracy_val_tensor = torch.cat([self.accuracy_val_tensor, accuracy_val])
+      self.auroc_val_tensor = torch.cat([self.auroc_val_tensor, auroc_val])
+      
       return loss
-  
-    def validation_epoch_end(self, validation_step_outputs):
-      avg_val_acc = torch.tensor(self.val_acc_list).mean()
-      avg_val_auroc = torch.tensor(self.val_auroc_list).mean()
 
-      self.val_acc_list.clear()
-      self.val_auroc_list.clear()
+    def validation_epoch_end(self, validation_step_outputs):
       
-      self.avg_val_acc_list.append(avg_val_acc.item())
-      self.avg_val_auroc_list.append(avg_val_auroc.item())
+      avg_loss_val = self.loss_val_tensor.mean().item()
+      avg_accuracy_val = self.accuracy_val_tensor.mean().item()
+      avg_auroc_val = self.auroc_val_tensor.mean().item()
       
+      self.loss_val_tensor = []
+      self.accuracy_val_tensor = []
+      self.auroc_val_tensor = []
+      
+      self.loss_val_list.append(avg_loss_val)
+      self.accuracy_val_list.append(avg_accuracy_val)
+      self.auroc_val_list.append(avg_auroc_val)
+
       #self.log("step",self.current_epoch)
-      
+
     def configure_optimizers(self):
       optimizer = optim.Adam(self.parameters(), lr=self.hparams.lr)
       return optimizer
-    
+
     def dice_coeff(self, input: Tensor, target: Tensor, reduce_batch_first: bool = False, epsilon: float = 1e-6):
     # Average of Dice coefficient for all batches, or for a single mask
       sum_dim = (-1, -2) if input.dim() == 2 or not reduce_batch_first else (-1, -2, -3)
@@ -325,7 +347,7 @@ class SegmentationModel(pl.LightningModule):
 
       dice = (inter + epsilon) / (sets_sum + epsilon)
       return dice.mean()
-  
+
 
 if __name__ == '__main__':
     import sys
@@ -338,26 +360,26 @@ if __name__ == '__main__':
     from torch.utils.data import DataLoader
     from utils.createCSV import unetCSV
     from pytorch_lightning import loggers as pl_loggers
-    
+
     config = {
         "batch_size":2,
         "img_size":512,
         "num_workers":2,
         "lr":1e-3,
     }
-    
-    
+
+
     # unetCSV("../../data/PALM-Training400",
     #         "../../data/PALM-Training400-Annotation-D&F/Disc_Masks",
     #         "../utils/customDataVgg.json",
     #         "../datasets/efficient_resunet/")
-    
+
     pl.seed_everything(42,workers=True)
 
-   
+
     train_features = UNETDataset("../datasets/efficient_resunet/Unet_train.csv",CustomTransformationResUnet(config["img_size"]))
     train_loader = DataLoader(train_features,batch_size=config["batch_size"],num_workers=config["num_workers"],shuffle=True)
-    
+
     val_dataset = UNETDataset("../datasets/efficient_resunet/Unet_val.csv",CustomTransformationResUnet(config["img_size"]))
     val_loader = DataLoader(val_dataset,batch_size=config["batch_size"],num_workers=config["num_workers"],shuffle=False)
 
@@ -376,9 +398,9 @@ if __name__ == '__main__':
                      filename="resunet-{epoch}-{train_val_acc:.2f}",
                      save_top_k=2,
                      monitor="train_val_loss")],
-        
+
         log_every_n_steps=40,
-        
+
         # limit_train_batches=1.0, limit_val_batches=1.0
         # resume_from_checkpoint="some/path/to/my_checkpoint.ckpt"
     )
